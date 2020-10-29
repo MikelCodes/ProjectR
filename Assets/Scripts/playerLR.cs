58958e1aacaaedc2149137a6b3f4852cb298c941
MikelCodes <mwaldie01@gmail.com>
2020-10-29 14:23:28 +1100

    version 0.0.0.1 I have a move script working but it is very janky. There is no move speed limit or drag. Currently only one player prefab.

---
+++ Assets/Scripts/playerLR.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerLR : MonoBehaviour
+{
+    //getting key binds
+
+    //forward key
+    [SerializeField]
+    KeyCode forward = KeyCode.D;
+
+    //backwards key
+    [SerializeField]
+    KeyCode backwards = KeyCode.A;
+    
+    //reference to rigidbody
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float moveSpeed;
+
+
+
+    void Update()
+    {
+        //slow down (a bit janky)
+
+        if (rb.velocity.x != 0)
+        {
+            rb.AddForce(-transform.forward*2);
+        }
+
+        //run keyPressed
+        keyPressed();
+    }
+
+    //keyPressed
+    void keyPressed()
+    {
+        //if pressing forward key
+        if (Input.GetKey(forward))
+        {
+            //run lookRight
+            lookRight();
+
+            //move forward
+            rb.AddForce(transform.forward * moveSpeed);
+        }
+
+        //if pressing backwards key
+        if (Input.GetKey(backwards))
+        {
+            //run lookLeft
+            lookLeft();
+
+            //move forward
+            rb.AddForce(transform.forward * moveSpeed);
+        }
+
+    }
+
+    //lookRight
+    void lookRight()
+    {
+        //Make look left
+        transform.rotation = Quaternion.Euler(transform.rotation.x, 90, transform.rotation.z);
+    }
+
+    //lookLeft
+    void lookLeft()
+    {
+        //Make look right
+        transform.rotation = Quaternion.Euler(transform.rotation.x, -90, transform.rotation.z);
+    }
+
+    
+}
---
+++ Assets/Scripts/playerUp.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerUp : MonoBehaviour
+{
+    //reference to rigidbody
+    [SerializeField]
+    private Rigidbody rb;
+
+    //jump key
+    [SerializeField]
+    KeyCode jump = KeyCode.W;
+
+    //distance of raycast
+    [SerializeField]
+    private float groundDist;
+
+    //seting the height of the jump
+    [SerializeField]
+    private float jumpHeight = 300;
+
+    // set jump timer
+    private float noJump;
+
+    //making a vector 3 for direction of ground
+    private Vector3 hitDir;
+
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //seting direction of raycast directly down
+        hitDir = new Vector3(0, -90, 0);
+
+        //jump timer
+        if (noJump > 0)
+        {
+            noJump -= Time.deltaTime;
+        }
+
+
+        //if pressing jump key
+        if (Input.GetKey(jump))
+        {
+            if (noJump <= 0)
+            {
+                //run void canJump
+                canJump();
+
+                //set timer
+                noJump = 0.25f;
+            }
+        }
+    }
+
+    void canJump()
+    {
+        //stores raycast hit data in 'hit'
+        RaycastHit hit;
+
+        //runs raycast from player position looking down. Saves data in hit and will only go as far as before jump.
+        if (Physics.Raycast(rb.transform.position, hitDir, out hit, groundDist))
+        {
+            //if what is hit has the "jumpGround" tag player can jump
+            if (hit.transform.tag == "jumpGround")
+            {
+                //jumps
+                rb.AddForce(transform.up * jumpHeight);
+            }
+        }
+        //else it slows down air movement (a bit janky)
+        else
+        {
+
+             rb.AddForce(-transform.forward * 100);
+            
+        }
+    }
+}
