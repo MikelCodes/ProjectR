9665825f707bd345a1a25624f212f641088967cc
MikelCodes <mwaldie01@gmail.com>
2020-10-30 20:42:24 +1100

    ver 0.0.0.3 Fixed even more movement and gave a delay before jump

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -14,6 +14,10 @@ public class playerMove : MonoBehaviour
     [SerializeField]
     KeyCode backwards = KeyCode.A;
 
+    //jump key
+    [SerializeField]
+    KeyCode jump = KeyCode.W;
+
     //reference to rigidbody
     [SerializeField]
     private Rigidbody rb;
@@ -21,9 +25,6 @@ public class playerMove : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
-    //jump key
-    [SerializeField]
-    KeyCode jump = KeyCode.W;
 
     //distance of raycast
     [SerializeField]
@@ -31,54 +32,38 @@ public class playerMove : MonoBehaviour
 
     //seting the height of the jump
     [SerializeField]
-    private float jumpHeight = 300;
+    private float jumpHeight = 1600;
 
     //setting the maximum speed of any character
     [SerializeField]
     private float maxSpeed = 30f;
 
+    [SerializeField]
+    private float gravity = 40f;
+
     // set jump timer
     private float noJump;
+
+    [SerializeField]
+    private float maxJumpDelay;
+
+    private float jumpDelay;
 
     //making a vector 3 for direction of ground
     private Vector3 hitDir;
 
+    private bool jumpPressed;
+
+    void Start()
+    {
+        //seting direction of raycast directly down
+        hitDir = new Vector3(0, -90, 0);
+    }
 
     void Update()
     {
-
-        //slow down (a bit janky)
-
-        if (rb.velocity.x != 0)
-        {
-            rb.AddForce(-transform.forward * 2);
-        }
-
         //run keyPressed
         keyPressed();
-
-        //seting direction of raycast directly down
-        hitDir = new Vector3(0, -90, 0);
-
-        //jump timer
-        if (noJump > 0)
-        {
-            noJump -= Time.deltaTime;
-        }
-
-
-        //if pressing jump key
-        if (Input.GetKey(jump))
-        {
-            if (noJump <= 0)
-            {
-                //run void canJump
-                canJump();
-
-                //set timer
-                noJump = 0.25f;
-            }
-        }
 
         // Trying to Limit Speed
         if (rb.velocity.magnitude > maxSpeed)
@@ -86,69 +71,91 @@ public class playerMove : MonoBehaviour
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
         }
 
-        //keyPressed
-        void keyPressed()
+        jumpDelay -= Time.deltaTime;
+
+        if (jumpPressed == true)
         {
-            //if pressing forward key
-            if (Input.GetKey(forward))
+            if (jumpDelay <= 0)
             {
-                //run lookRight
-                lookRight();
-
-                //move forward
-                rb.AddForce(transform.forward * moveSpeed);
-            }
-
-            //if pressing backwards key
-            if (Input.GetKey(backwards))
-            {
-                //run lookLeft
-                lookLeft();
-
-                //move forward
-                rb.AddForce(transform.forward * moveSpeed);
-            }
-
-        }
-
-        //lookRight
-        void lookRight()
-        {
-            //Make look left
-            transform.rotation = Quaternion.Euler(transform.rotation.x, 90, transform.rotation.z);
-        }
-
-        //lookLeft
-        void lookLeft()
-        {
-            //Make look right
-            transform.rotation = Quaternion.Euler(transform.rotation.x, -90, transform.rotation.z);
-        }
-
-
-
-        void canJump()
-        {
-            //stores raycast hit data in 'hit'
-            RaycastHit hit;
-
-            //runs raycast from player position looking down. Saves data in hit and will only go as far as before jump.
-            if (Physics.Raycast(rb.transform.position, hitDir, out hit, groundDist))
-            {
-                //if what is hit has the "jumpGround" tag player can jump
-                if (hit.transform.tag == "jumpGround")
-                {
-                    //jumps
-                    rb.AddForce(transform.up * jumpHeight);
-                }
-            }
-            //else it slows down air movement (a bit janky)
-            else
-            {
-
-                rb.AddForce(-transform.forward * 100);
-
+                //jumps
+                rb.AddForce(transform.up * jumpHeight);
+                jumpPressed = false;
             }
         }
     }
+
+    //keyPressed
+    void keyPressed()
+    {
+        //if pressing forward key
+        if (Input.GetKey(forward))
+        {
+            //run lookRight
+            lookRight();
+
+            //move forward
+            rb.AddForce(transform.forward * moveSpeed);
+        }
+
+        //if pressing backwards key
+        if (Input.GetKey(backwards))
+        {
+            //run lookLeft
+            lookLeft();
+
+            //move forward
+            rb.AddForce(transform.forward * moveSpeed);
+        }
+
+
+        //jump timer
+        if (noJump > 0)
+        {
+            noJump -= Time.deltaTime;
+        }
+
+        //stores raycast hit data in 'hit'
+        RaycastHit hit;
+
+        //runs raycast from player position looking down. Saves data in hit and will only go as far as before jump.
+        if (Physics.Raycast(rb.transform.position, hitDir, out hit, groundDist))
+        {
+            //if what is hit has the "jumpGround" tag player can jump
+            if (hit.transform.tag == "jumpGround")
+            {
+                //if pressing jump key
+                if (Input.GetKey(jump))
+                {
+                    if (noJump <= 0)
+                    {
+                        //set timer
+                        noJump = (maxJumpDelay + 0.25f);
+                        jumpDelay = maxJumpDelay;
+                        jumpPressed = true;
+                    }
+
+                }
+            }
+        }
+        // makes player fall faster
+        else
+        {
+            rb.AddForce(-transform.up * gravity);
+        }
+    }
+
+    //lookRight
+    void lookRight()
+    {
+        //Make look left
+        transform.rotation = Quaternion.Euler(transform.rotation.x, 90, transform.rotation.z);
+    }
+
+    //lookLeft
+    void lookLeft()
+    {
+        //Make look right
+        transform.rotation = Quaternion.Euler(transform.rotation.x, -90, transform.rotation.z);
+    }
+
 }
